cacf8a855a268a1fa782dfd900741959d0ab3455
Arithmetic <2637691052@qq.com>
2020-03-23 15:39:53 +0800

    //随机森林分类。

---
+++ src/main/scala/com/lc/study/no5classification/n1classification/RandomDorestClassifierDemo.scala
@@ -0,0 +1,16 @@
+package com.lc.study.no5classification.n1classification
+/*
+随机森林。
+决策树分类算法，可以很好的对类别进行分类，但是，前提条件是我们对该书的叶子，也就是其分类因子进行了减少
+并且能保证这些分类因子可以准确的表达分类目标。
+但是再生产中，这种数据其实很少。那么就容易造成过拟合问题。过拟合问题就是在训练数据的预测中错误率很低
+而在测试数据中错误率很高。
+为了解决这个问题，随意森林算法就出现了，它有多个决策树组成。综合其结果，进行分类预测。
+其使用过程和决策树的使用过程大差不差。
+
+https://blog.csdn.net/w952470866/article/details/78987265/
+
+ */
+object RandomDorestClassifierDemo extends App {
+
+}
