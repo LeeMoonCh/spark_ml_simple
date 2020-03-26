0d068748dce2a1b9dea8632d7740c3b194c9a483
Arithmetic <2637691052@qq.com>
2020-03-26 13:54:59 +0800

    //MLPC数据集

---
+++ src/main/scala/com/lc/study/no5classification/n1classification/MultilayerPerceptronClassifierDemo.scala
@@ -0,0 +1,31 @@
+package com.lc.study.no5classification.n1classification
+/*
+MLPC  ==> 多层感知机分类器。也叫做前瞻性人工神经网络分类器(ANN)
+MLP 分为三层：输入层、一个或多个隐藏层，和输出层。
+MLPC 的输入层按照特征数进行确定（比如，你有10个特征，那么你就有10个节点的输入层)
+输出层，按照分类结果进行设置(比如，你有2个分类结果，那么就将输出层设置成2个节点)
+
+输入层使用逻辑回归方法进行激活(sigmoid函数。)
+输出层使用softmax算法进行计算。
+
+ */
+object MultilayerPerceptronClassifierDemo extends App{
+
+
+  import com.lc.study.spark
+
+  import spark.implicits._
+
+  //官网的例子是一个
+
+
+
+
+
+
+
+
+
+
+
+}
